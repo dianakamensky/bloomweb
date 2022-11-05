bff6a277df559e2eccc832de39d829ed63c4f9cb
Diana Kamensky <dianakamensky@gmail.com>
2022-11-05 21:21:10 +0200

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import "./index.css";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import ErrorPage from "./error-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <Index />
+          }
+        ]
+      }
+    ]
+  }
+]
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
---
+++ src/routes/root.jsx
@@ -0,0 +1,10 @@
+import { Outlet, NavLink, useLoaderData, Form, redirect, useNavigation, useSubmit } from "react-router-dom";
+import { useEffect } from "react";
+
+export default function Root() {
+  return (
+    <Header></Header>
+    
+
+    )
+}
